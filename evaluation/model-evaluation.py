616b0fc4f24c33700a028bba028fcc4b36d3f70c
Daniel Peng <peng.daniel11440@gmail.com>
2020-11-17 15:59:26 -0500

    correct f-score calculation and update graphs to be correct

--- evaluation/model-evaluation.py
+++ evaluation/model-evaluation.py
@@ -9,8 +9,8 @@ def chrF(real_text, generated_text):
     # print()
     generated_text_chars = set(generated_text)
     real_text_chars = set(real_text)
-    precision = len([0 for c in generated_text if c in real_text_chars])    # fraction of n-grams present in the generated text that are also in the real text
-    recall = len([0 for c in real_text if c in generated_text_chars])       # fraction of n-grams present in the real text that are also in the generated text
+    precision = len([0 for c in generated_text if c in real_text_chars])/len(generated_text)    # fraction of n-grams present in the generated text that are also in the real text
+    recall = len([0 for c in real_text if c in generated_text_chars])/len(real_text)       # fraction of n-grams present in the real text that are also in the generated text
     if precision + recall == 0:
         return 0
     return 2*(precision * recall) / (precision + recall)
@@ -18,8 +18,8 @@ def chrF(real_text, generated_text):
 def wordF(real_text, generated_text):
     real_words = real_text.split()
     gen_words = generated_text.split()
-    precision = len([0 for w in gen_words if w in real_words])    # fraction of n-grams present in the generated text that are also in the real text
-    recall = len([0 for w in real_words if w in gen_words])    # fraction of n-grams present in the generated text that are also in the real text
+    precision = len([0 for w in gen_words if w in real_words])/len(gen_words)    # fraction of n-grams present in the generated text that are also in the real text
+    recall = len([0 for w in real_words if w in gen_words])/len(real_words)    # fraction of n-grams present in the generated text that are also in the real text
     if precision + recall == 0:
         return 0
     return 2*(precision * recall) / (precision + recall)
@@ -34,13 +34,10 @@ GPT2_NEWS_OUTPUT_FILE_NAME = '../GPT-2/news_output'
 LSTM_TRUMP_OUTPUT_FILE_NAME = '../LSTM/test-outputs/LSTM_trump_test_output'
 LSTM_NEWS_OUTPUT_FILE_NAME = '../LSTM/test-outputs/LSTM_news_test_output'
 
-# LSTM_TRUMP_OUTPUT_FILE_NAME = '../LSTM/test-outputs/trump-for-different-temperatures/output-0.2.txt'
-# LSTM_NEWS_OUTPUT_FILE_NAME = '../LSTM/test-outputs/LSTM_news_test_output'
-
 trump_set = (ACTUAL_TRUMP_TWEETS_FILE_NAME, GPT2_TRUMP_OUTPUT_FILE_NAME, LSTM_TRUMP_OUTPUT_FILE_NAME, 'Trump')
 news_set = (ACTUAL_NEWS_TWEETS_FILE_NAME, GPT2_NEWS_OUTPUT_FILE_NAME, LSTM_NEWS_OUTPUT_FILE_NAME, 'News')
 
-def get_lstm_graphs():
+def generate_graphs(model_type):
     temperatures = [0.2, 0.4, 0.6, 0.8, 1.0, 1.2]
     avg_chrF_scores = []
     avg_wordF_scores = []
@@ -48,35 +45,41 @@ def get_lstm_graphs():
         # real_lines = [l[:-1] for l in real_file.read().split('\n') if not l.isspace()]
         real_lines = [l[:-1].lower() for l in f.read().split('\n') if l.strip() != '']
     for temperature in temperatures:
-        with open(f'../LSTM/test-outputs/trump-for-different-temperatures/output-{temperature}.txt') as f:
-            lstm_lines = [l[:-1].lower() for l in f.read().split('\n') if l.strip() != '']
+        file_name = f'../LSTM/test-outputs/trump-for-different-temperatures/output-{temperature}.txt' if model_type == 'LSTM' else \
+                    f'../GPT-2/temperature_outputs/trump_output_{str(f"0{str(int(temperature*10))}")[-2:]}'
+        with open(file_name) as f:
+            gen_lines = [l[:-1].lower() for l in f.read().split('\n') if l.strip() != '']
         chrF_scores = []
         wordF_scores = []
         for i in range(len(real_lines)):
+
             sixth_word_start_index = sum([len(w) for w in real_lines[i].split()[:5]]) + 4 # 4 spaces
-            real_tweet, lstm_tweet = real_lines[i][sixth_word_start_index:], lstm_lines[i][sixth_word_start_index:]
-            chrF_scores.append(chrF(real_tweet, lstm_tweet))
-            wordF_scores.append(wordF(real_tweet, lstm_tweet))
+            real_tweet, gen_tweet = real_lines[i][sixth_word_start_index:], gen_lines[i][sixth_word_start_index:]
+            if real_tweet == '' or gen_tweet == '':
+                continue
+            chrF_scores.append(chrF(real_tweet, gen_tweet))
+            wordF_scores.append(wordF(real_tweet, gen_tweet))
         avg_chrF_scores.append(sum(chrF_scores)/len(chrF_scores))
         avg_wordF_scores.append(sum(wordF_scores)/len(wordF_scores))
     # plot
     fig1, ax1 = plt.subplots()
     ax1.plot(temperatures, avg_chrF_scores)
-    ax1.set_title('LSTM: Temperature vs chrF')
+    ax1.set_title(f'{model_type}: Temperature vs chrF')
     ax1.set_xlabel('Temperature')
     ax1.set_ylabel('chrF score')
-    fig1.savefig('LSTM-temperature-vs-chrF.png')
+    fig1.savefig(f'{model_type}-temperature-vs-chrF.png')
 
 
     fig2, ax2 = plt.subplots()
     ax2.plot(temperatures, avg_wordF_scores)
-    ax2.set_title('LSTM: Temperature vs wordF')
+    ax2.set_title(f'{model_type}: Temperature vs word ngram F-score')
     ax2.set_xlabel('Temperature')
     ax2.set_ylabel('wordF score')
-    fig2.savefig('LSTM-temperature-vs-wordF.png')
+    fig2.savefig(f'{model_type}-temperature-vs-wordF.png')
 
 
-get_lstm_graphs()
+generate_graphs('LSTM')
+generate_graphs('GPT-2')
 
 for real_file, gpt_file, lstm_file, tweet_user in (trump_set, news_set):
     print('\n' + tweet_user)
@@ -92,8 +95,11 @@ for real_file, gpt_file, lstm_file, tweet_user in (trump_set, news_set):
     gpt_wordF_scores, lstm_wordF_scores = [], []
     for i in range(len(real_lines)):
         sixth_word_start_index = sum([len(w) for w in real_lines[i].split()[:5]]) + 4 # 4 spaces
-        min_len = min(len(real_lines[i]), len(lstm_lines[i]), len(gpt_lines[i]))
+        # min_len = min(len(real_lines[i]), len(lstm_lines[i]), len(gpt_lines[i]))
+        min_len = 10000
         real_tweet, lstm_tweet, gpt_tweet = real_lines[i][sixth_word_start_index:min_len], lstm_lines[i][sixth_word_start_index:min_len], gpt_lines[i][sixth_word_start_index:min_len]
+        if '' in (real_tweet, lstm_tweet, gpt_tweet):
+            continue
         gpt_chrF_scores.append(chrF(real_tweet, gpt_tweet))
         lstm_chrF_scores.append(chrF(real_tweet, lstm_tweet))
         gpt_wordF_scores.append(wordF(real_tweet, gpt_tweet))
@@ -108,5 +114,4 @@ for real_file, gpt_file, lstm_file, tweet_user in (trump_set, news_set):
     lstm_avg_wordF = sum(lstm_wordF_scores) / len(lstm_wordF_scores)
     print('LSTM wordF score:\t', lstm_avg_wordF, '\tstddev:', statistics.stdev(lstm_wordF_scores))
     print('GPT-2 wordF score:\t', gpt_avg_wordF, '\tstddev:', statistics.stdev(gpt_wordF_scores))
-
 
